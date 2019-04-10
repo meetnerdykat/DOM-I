e7bc4ab62b68fc96bf8f3ad704eee6ef8de093c6
Kathy Cuevas <meetnerdykat@gmail.com>
2019-04-10 16:58:10 -0400

    completed whole project except for adding new elements. It is coming up next

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const siteContent = {
   },
 
   cta: {
-    h1: 'DOM Is Awesome',
+    h1: 'DOM\n Is\n Awesome',
     button: 'Get Started',
     'img-src': 'img/header-img.png'
   },
@@ -46,7 +46,7 @@ const siteContent = {
 
   contact: {
     'contact-h4': 'Contact',
-    address: '123 Way 456 Street Somewhere, USA',
+    address: '123 Way 456 Street\nSomewhere, USA',
     phone: '1 (888) 888-8888',
     email: 'sales@greatidea.io'
   },
@@ -77,13 +77,84 @@ ctaImage.setAttribute('src', siteContent['cta']['img-src']);
 // make the H1 appear
 let ctaHeading = document.querySelector('h1');
 ctaHeading.textContent = siteContent['cta']['h1'];
+ctaHeading.style.whiteSpace = 'pre-wrap';
 
 // make the button text appear
 let ctaButton = document.querySelector('button');
 ctaButton.textContent = siteContent['cta']['button'];
 
 // make features h4 appear
+let featuresH4 = document.querySelector('div:nth-child(1) > h4');
+featuresH4.textContent = siteContent['main-content']['features-h4'];
+
+// make features paragraph appear
+let featuresP = document.querySelector('div:nth-child(1) > p');
+featuresP.textContent = siteContent['main-content']['features-content'];
+
+// make about h4 appear
+let aboutH4 = document.querySelector('div:nth-child(2) > h4');
+aboutH4.textContent = siteContent['main-content']['about-h4'];
+
+// make about paragraph appear
+let aboutP = document.querySelector('div:nth-child(2) > p');
+aboutP.textContent = siteContent['main-content']['about-content'];
 
 // make the middle image appear
 let middleImage = document.getElementById('middle-img');
 middleImage.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+// make the services h4 appear
+let servicesH4 = document.querySelector(
+  'div.bottom-content > div:nth-child(1) > h4'
+);
+servicesH4.textContent = siteContent['main-content']['services-h4'];
+
+// make the services paragraph appear
+let servicesP = document.querySelector(
+  'div.bottom-content > div:nth-child(1) > p'
+);
+servicesP.textContent = siteContent['main-content']['services-content'];
+
+// // make the product h4 appear
+let productH4 = document.querySelector(
+  'div.bottom-content > div:nth-child(2) > h4'
+);
+productH4.textContent = siteContent['main-content']['product-h4'];
+
+// // make the product paragraph appear
+let productP = document.querySelector(
+  'div.bottom-content > div:nth-child(2) > p'
+);
+productP.textContent = siteContent['main-content']['product-content'];
+
+// // make the vision h4 appear
+let visionH4 = document.querySelector(
+  'div.bottom-content > div:nth-child(3) > h4'
+);
+visionH4.textContent = siteContent['main-content']['vision-h4'];
+
+// // make the vision paragraph appear
+let visionP = document.querySelector(
+  'div.bottom-content > div:nth-child(3) > p'
+);
+visionP.textContent = siteContent['main-content']['vision-content'];
+
+// make the contact h4 appear
+let contactH4 = document.querySelector('section.contact > h4');
+contactH4.textContent = siteContent['contact']['contact-h4'];
+
+// make the paragraph 1-address appear
+let p1Address = document.querySelector('section.contact > p:nth-child(2)');
+p1Address.textContent = siteContent['contact']['address'];
+p1Address.style.whiteSpace = 'pre-wrap';
+
+// make the paragraph 2-phone appear
+let p2Phone = document.querySelector('section.contact > p:nth-child(3)');
+p2Phone.textContent = siteContent['contact']['phone'];
+
+// make the paragraph 3-email appear
+let p3Email = document.querySelector('section.contact > p:nth-child(4)');
+p3Email.textContent = siteContent['contact']['email'];
+
+let footerText = document.querySelector('footer > p');
+footerText.textContent = siteContent['footer']['copyright'];
